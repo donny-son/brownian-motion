cdb2c87c1dfa73c2ef54ee87bef70dbdf2d19a20
Donny Son <dongook.son@yonsei.ac.kr>
2020-07-08 18:16:36 +0900

    code cleanup

--- brownian_motion.py
+++ brownian_motion.py
@@ -9,24 +9,23 @@ class BrownianMotion:
         self.res = 0
         self.x = 0
         self.y = 0
-        self.array
 
     def result(self):
         for i in range(self.n_times):
             if i == 0:
                 self.array[i,] = self.time, self.res, self.x, self.y
-                continue
-            self.time = i
-            self.res = numpy.random.binomial(1,0.5)
-            if self.res == 1:
-                self.x += 1
-                self.y += 1
-
             else:
-                self.x += 1
-                self.y -= 1
+                self.time = i
+                self.res = numpy.random.binomial(1,0.5)
+                if self.res == 1:
+                    self.x += 1
+                    self.y += 1
 
-            self.array[i,] = self.time, self.res, self.x, self.y
+                else:
+                    self.x += 1
+                    self.y -= 1
+
+                self.array[i,] = self.time, self.res, self.x, self.y
 
         return self.array
 
